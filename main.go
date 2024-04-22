a7fa9d98c0b915b4b0c76a3e1589a6ac555af90e
Bernardo <bernardo@linuxadmin.cloud>
2024-04-22 07:48:47 -0300

    feat: edit message

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hi"))
+		w.Write([]byte("Welcome home :)"))
 	})
 
 	r.Route("/todos", func(r chi.Router) {
